a8053e13cabd76d4c8daf6f6c3cfefea7d6d7502
skiloop <skiloop@gmail.com>
2023-02-08 18:16:13 +0800

    test: add proxy test, header test and json test for Curl

--- tests/curl_tests.py
+++ tests/curl_tests.py
@@ -1,30 +1,54 @@
 import logging
 import unittest
+from copy import deepcopy
 
 import pycurl
+from proxy import proxy
 
 from curlreq import Curl, Request, version
 
 METHODS_WITH_BODY = ["PUT", "PATCH", "POST", "DELETE"]
 
 
-def test_request(case, curl, method, url, body, **kwargs):
-    data = body if method in METHODS_WITH_BODY else None
-    req = Request(url, method, data=data)
-    resp = curl.do_req(req.prepare(), **kwargs)
+def test_with_prepared_request(case, curl, req, method, **kwargs):
+    resp = curl.do_req(req, **kwargs)
     case.assertFalse(resp is None, f"[{method}] response is None")
     if resp.status_code == 405:
-        return
+        return resp
     case.assertTrue(resp.status_code in [200, 204],
                     f"[{method}] status code failed: expected: 200, but go {resp.status_code}")
     if resp.status_code == 204:
-        return
+        return resp
     if method not in ["GET", "TRACE"] and method not in METHODS_WITH_BODY:
         case.assertTrue(resp.content == b"", f"[{method}] content not empty")
-        return
+        return resp
     rspj = resp.json
     case.assertTrue(rspj["method"] == method,
                     f"[{method}]method failed: expected: {method} got {rspj['method']}")
+    return resp
+
+
+def test_request(case, curl, method, url, body, **kwargs):
+    data = body if method in METHODS_WITH_BODY else None
+    req = Request(
+        url,
+        method,
+        data=data,
+        json=kwargs.pop('json', None),
+        files=kwargs.pop('files', None),
+        headers=kwargs.pop('headers', None),
+        cookies=kwargs.pop('cookies', None),
+        auth=kwargs.pop('auth', None),
+        params=kwargs.pop('params', None),
+    )
+    return test_with_prepared_request(case, curl, req.prepare(), method, **kwargs)
+
+
+def check_dict(dst: dict, src: dict):
+    for key, val in dst.items():
+        if val != src.get(key):
+            return False
+    return True
 
 
 class CurlTests(unittest.TestCase):
@@ -89,6 +113,27 @@ class CurlTests(unittest.TestCase):
         self.curl.setopt(pycurl.HTTP_VERSION, pycurl.CURL_HTTP_VERSION_3)
         test_request(self, self.curl, "GET", self.test_https_url, self.dict_body, **self.options)
         test_request(self, self.curl, "POST", self.test_https_url, self.dict_body, **self.options)
+
+    def testProxy(self):
+        with proxy.Proxy(["--log-level=CRITICAL", "--threadless"]):
+            self.curl.setopt(pycurl.HTTP_VERSION, pycurl.CURL_HTTP_VERSION_1_1)
+            options = deepcopy(self.options)
+            options["proxies"] = {"https": "http://localhost:8899"}
+            test_request(self, self.curl, "GET", self.test_https_url, self.dict_body, **options)
+            test_request(self, self.curl, "POST", self.test_https_url, self.dict_body, **options)
+
+    def testPostFormat(self):
+        self.curl.setopt(pycurl.HTTP_VERSION, pycurl.CURL_HTTP_VERSION_1_1)
+        resp = test_request(self, self.curl, "POST", self.test_https_url, None, json=self.dict_body)
+        self.assertTrue(resp.json["json"] and len(resp.json['json']) == len(self.dict_body),
+                        f"json body not setting right: {str(resp.json['json'])}")
+
+    def testHeaders(self):
+        self.curl.setopt(pycurl.HTTP_VERSION, pycurl.CURL_HTTP_VERSION_1_1)
+        headers = {"Acek": "jjsp", "Doit": "yes"}
+        resp = test_request(self, self.curl, "GET", self.test_https_url, None, headers=headers)
+        self.assertTrue(resp.json["headers"] and check_dict(headers, resp.json["headers"]),
+                        f"json body not setting right: {str(resp.json['headers'])}")
 
 
 if __name__ == '__main__':
